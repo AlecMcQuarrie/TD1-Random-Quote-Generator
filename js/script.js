89b284d51c80ea97bfbec2794afefc1ed3d7214f
Alec McQuarrie <awesome4lec@gmail.com>
2020-09-01 10:22:04 -0400

    Final fix and touch up
    Added null instead of empty strings, so it is easier to identify if there is actually no year or not

--- js/script.js
+++ js/script.js
@@ -22,64 +22,64 @@ let quotes = [
     {
         quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",
         source: "Marilyn Monroe",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "funny, relatable"
     },
     {
         quote: "Be yourself; everyone else is already taken.",
         source: "Oscar Wilde",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "funny"
     },
     {
         quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
         source: "Albert Einstein",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "deep, funny, einstein"
     },
     {
         quote: "So many books, so little time.",
         source: "Frank Zappa",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "books, deep"
     },
     {
         quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
         source: "Bernard M. Baruch",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "inspiring"
     },
     {
         quote: "A room without books is like a body without a soul.",
         source: "Marcus Tullius Cicero",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "deep, books"
     },
     {
         quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
         source: "Dr. Seuss",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "relatable, deep, funny"
     },
     {
         quote: "You only live once, but if you do it right, once is enough.",
         source: "Mae West",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "funny, relatable"
     },
     {
         quote: "Be the change that you wish to see in the world.",
         source: "Mahatma Gandhi",
-        citation: " ",
-        year: " ",
+        citation: null,
+        year: null,
         tags: "inspiring, deep, gandhi"
     }
 ];
@@ -113,15 +113,15 @@ function getRandomQuote() {
 function printQuote() {
     let quote = getRandomQuote();
     
-    if (quote.citation === " " && quote.year !== " ") {
+    if (quote.citation === null && quote.year !== null) {
         document.getElementById("quote-box").innerHTML = `<p class="quote">${quote.quote}</p>
         <p class="source">${quote.source}<span class="year">${quote.year}</span></p>
         <p>Tags: ${quote.tags}</p>`;
-    } else if (quote.citation !== " " && quote.year === " ") {
+    } else if (quote.citation !== null && quote.year === null) {
         document.getElementById("quote-box").innerHTML = `<p class="quote">${quote.quote}</p>
         <p class="source">${quote.source}<span class="citation">${quote.citation}</span></p>
         <p>Tags: ${quote.tags}</p>`;
-    } else if (quote.citation !== " " && quote.year === " ") {
+    } else if (quote.citation === null && quote.year === null) {
         document.getElementById("quote-box").innerHTML = `<p class="quote">${quote.quote}</p>
         <p class="source">${quote.source}</p>
         <p>Tags: ${quote.tags}</p>`;
